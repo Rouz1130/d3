c170a34d5375afaca2cdac23489c1844c939f515
unknown <rmajlessi@gmail.com>
2018-02-28 01:32:54 -0800

    Add ngx charts, chart.html, import statments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgxChartsModule} from '@swimlane/ngx-charts';
+import {multi, single} from '../data';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  template: `
+    <ngx-charts-tree-map
+      [view]="view"
+      [scheme]="colorScheme"
+      [results]="single"
+      (select)="onSelect($event)">
+    </ngx-charts-tree-map>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  single: any[];
+  multi: any[];
+
+  view: any[] = [700, 400];
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  constructor() {
+    Object.assign(this, {single})   
+    console.log(single)
+  }
+  
+  onSelect(event) {
+    console.log(event);
+  }
+  
 }
